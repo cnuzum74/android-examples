9e80ff19bf08c4fa615d4e61464e75cce5c716f0
cnuzum74 <cnuzum74@gmail.com>
2015-03-14 17:28:47 -0700

    This is another line of text to text git commits.

--- receipteditexample/ReceiptEditExample/src/main/java/com/clover/example/receipteditexample/ReceiptRegistrationProvider.java
+++ receipteditexample/ReceiptEditExample/src/main/java/com/clover/example/receipteditexample/ReceiptRegistrationProvider.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * Adding text to test diff.
+ * One more text line.
  */
 package com.clover.example.receipteditexample;
 
